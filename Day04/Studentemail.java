ee86e0d1eba424b1ee840ad36a8a1937eafb9b41
Karthikeyan M <rdjmk1037@gmail.com>
2025-06-19 19:40:46 +0530

    Add files via upload

---
+++ Day04/Studentemail.java
@@ -0,0 +1,14 @@
+package Day06;
+import java.util.HashSet;
+public class Studentemail {
+	public static void main(String[]args) {
+		HashSet<String> stuemail=new HashSet();
+		stuemail.add("rdjmk@gmail.com");
+		stuemail.add("karthik@gmail.com");
+		
+		for(String data:stuemail) {
+			System.out.println(data);
+		}
+	}
+
+}
